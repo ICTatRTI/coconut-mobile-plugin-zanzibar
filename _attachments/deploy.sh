9fbcdfd8935b4539258e4258e2b414bb964ba940
esurface <esurface@rti.org>
2022-03-01 17:17:52 -0500

    Add preventative measures on error to deployment scripts

--- _attachments/deploy.sh
+++ _attachments/deploy.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 echo "Browserifying, uglifying and then making plugin-bundle.js"
-./node_modules/browserify/bin/cmd.js --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee | npx terser > plugin-bundle.js
+rm plugin-bundle.js bundle.js
+./node_modules/browserify/bin/cmd.js --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee -o bundle.js
 
-echo "Pushing to cloud"
-#couchapp push --no-atomic cloud
-userpass=$(cat userpass.txt) 
-coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co zanzibar
+if [[ $? -eq 0 ]]; then
+  npx terser bundle.js > plugin-bundle.js
+  echo "Pushing to cloud"
+  userpass=$(cat userpass.txt) 
+  coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co zanzibar
+fi
--- _attachments/deployDevelopmentInstance.sh
+++ _attachments/deployDevelopmentInstance.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 echo "Browserifying, NOT uglifying and then making plugin-bundle.js"
-./node_modules/browserify/bin/cmd.js --debug --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee > plugin-bundle.js
-echo "Pushing to cloud zanzibar-development"
-#couchapp push --no-atomic --docid "_design/plugin-zanzibar-development" development
+./node_modules/browserify/bin/cmd.js --debug --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee -o plugin-bundle.js
 
-userpass=$(cat userpass.txt) 
-coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co zanzibar-development
+if [[ $? -eq 0 ]]; then
+  echo "Pushing to cloud zanzibar-development"
+  userpass=$(cat userpass.txt) 
+  coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co zanzibar-development
+fi
