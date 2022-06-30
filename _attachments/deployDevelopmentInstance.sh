aaae6eab31b8dc47b92d489e4af286e45f0119e5
esurface <esurface@rti.org>
2022-06-30 14:13:37 -0400

    make deploy.sh parameterized; copy Case.coffee and GeoHierarchy.coffee into the plugin

--- _attachments/deployDevelopmentInstance.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-echo "Browserifying, NOT uglifying and then making plugin-bundle.js"
-./node_modules/browserify/bin/cmd.js --debug --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee -o plugin-bundle.js
-
-if [[ $? -eq 0 ]]; then
-  echo "Pushing to cloud zanzibar-development"
-  userpass=$(cat userpass.txt) 
-  coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co zanzibar-development
-fi
