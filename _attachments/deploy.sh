610b286af146248150fb0c050da0d4cf3e8638cf
esurface <esurface@rti.org>
2022-06-30 14:23:13 -0400

    make deploy.sh parameterized

--- _attachments/deploy.sh
+++ _attachments/deploy.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+if [ $# -eq 0 ]; then
+  echo "Must specify database: ./deploy.sh <database>"
+  exit -1
+fi
+
 echo "Browserifying, uglifying and then making plugin-bundle.js"
-rm plugin-bundle.js bundle.js
-./node_modules/browserify/bin/cmd.js --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee -o bundle.js
+./node_modules/browserify/bin/cmd.js --debug --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee -o plugin-bundle.js
 
 if [[ $? -eq 0 ]]; then
-  npx terser bundle.js > plugin-bundle.js
   echo "Pushing to cloud"
   userpass=$(cat userpass.txt) 
-  coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co zanzibar
+  database=$1
+  coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co $database
 fi
