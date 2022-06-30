7753d2f97b29bb09cc45f52c9b07ed3ddf0103b4
esurface <esurface@rti.org>
2022-06-30 14:22:37 -0400

    Revert "make deploy.sh parameterized; copy Case.coffee and GeoHierarchy.coffee into the plugin"
    This reverts commit aaae6eab31b8dc47b92d489e4af286e45f0119e5.

--- _attachments/deploy.sh
+++ _attachments/deploy.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
-
-if [ $# -eq 0 ]; then
-  echo "Must specify database: ./deploy.sh <database>"
-  exit -1
-fi
-
 echo "Browserifying, uglifying and then making plugin-bundle.js"
-./node_modules/browserify/bin/cmd.js --debug --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee -o plugin-bundle.js
+rm plugin-bundle.js bundle.js
+./node_modules/browserify/bin/cmd.js --standalone Plugin -v -t coffeeify --extension='.coffee' Plugin.coffee -o bundle.js
 
 if [[ $? -eq 0 ]]; then
+  npx terser bundle.js > plugin-bundle.js
   echo "Pushing to cloud"
   userpass=$(cat userpass.txt) 
-  database=$1
-  coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co $database
+  coffee pushToCouchDB.coffee https://$userpass@zanzibar.cococloud.co zanzibar
 fi
